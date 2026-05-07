8e17b2f60ca7b0811f9d257730da19ce4aa435c9
DaFuHao <108219560+3257085208@users.noreply.github.com>
2026-05-08 04:10:24 +0800

    no

--- src/utils/nodeKey.ts
+++
@@ -1,9 +0,0 @@
-import type { Node } from '../types'
-
-export function nodeKeyFrom(source: string, uuid: string) {
-  return `${source}::${uuid}`
-}
-
-export function nodeKey(node: Node) {
-  return nodeKeyFrom(node.source, node.uuid)
-}
